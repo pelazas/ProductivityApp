5f6e2a3cebb9d31e4d94d9e74241f7b745262d29
UO288056 <UO288056@uniovi.es>
2023-11-19 14:05:30 +0100

    added transition between fragments

--- app/src/main/java/com/example/productivityapp/FragmentManager.java
+++ app/src/main/java/com/example/productivityapp/FragmentManager.java
@@ -13,6 +13,7 @@ public class FragmentManager {
 
     public void replaceFragment(Fragment fragment, boolean addToBackStack) {
         FragmentTransaction transaction = activity.getSupportFragmentManager().beginTransaction();
+        transaction.setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out);
         transaction.replace(R.id.fragment_container, fragment);
 
         if (addToBackStack) {
